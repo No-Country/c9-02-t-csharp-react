f7a22e3bc550ef0041add60c8295b2b5f6fbe076
FZ-developer <work.fzdeveloper@gmail.com>
2023-02-06 22:47:31 -0300

    Initial back commit

---
+++ back/MoonBankAPI/MoonBankAPI/Startup.cs
@@ -0,0 +1,29 @@
+﻿
+namespace MoonBankAPI
+{
+    public class Startup
+    {
+
+        private readonly IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // agregar servicios aquí
+
+
+            //services.AddDbContext<MoonBankContext>(option => option.UseSqlServer(_configuration.GetConnectionString("MoonBankSQL")));
+
+
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            // configurar la aplicación aquí
+        }
+    }
+}
