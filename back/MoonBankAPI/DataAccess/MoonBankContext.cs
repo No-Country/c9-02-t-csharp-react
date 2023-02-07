9a2418b671c55c47e718c19c03e28c1b8bac31f7
Emanuel Barile <emanuelbarile06@gmail.com>
2023-02-06 23:16:13 -0300

    context, class libraries, project references and nuget packages added

---
+++ back/MoonBankAPI/DataAccess/MoonBankContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace DataAccess
+{
+    public class MoonBankContext : DbContext
+    {
+
+        public MoonBankContext(DbContextOptions<MoonBankContext> options) : base(options) { }
+
+        
+
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+           
+
+        }
+
+    }
+}
