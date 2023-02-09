30f9c5f102038b824e199e846401a9a2b7ea7adc
Luis Mera <luismera86@gmail.com>
2023-02-08 22:05:46 -0300

    Interfaces and enums added

---
+++ front/MoonBank/src/shared/interfaces/Account.ts
@@ -0,0 +1,12 @@
+
+export interface Account {
+  id: number;
+  name: string;
+  lasName: string;
+  email: string;
+  password: string;
+  alias: string;
+  cbu_cvu: number;
+  balance: number;
+  rewardPoints: number;
+}
