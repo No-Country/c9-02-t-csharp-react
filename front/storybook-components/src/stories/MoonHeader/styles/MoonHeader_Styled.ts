4084aafb6a954f972884199f7dc7ff2fee95ef1c
SortOmega <sortocarlo755@gmail.com>
2023-02-22 17:11:42 -0600

    Create Moon Header

---
+++ front/storybook-components/src/stories/MoonHeader/styles/MoonHeader_Styled.ts
@@ -0,0 +1,50 @@
+import styled from 'styled-components';
+
+export const MoonHeader_Styled = styled.header`
+  display: flex;
+  box-sizing: border-box;
+  z-index: 200;
+  position: fixed;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  height: 55px;
+  color: #ffffff;
+  background: #090909;
+
+  > .Header__MenuBTN,
+  > .Header__AppName,
+  > .Header__NotificationBTN {
+    display: flex;
+    > input {
+      display: none;
+    }
+  }
+
+  > .Header__MenuBTN,
+  > .Header__NotificationBTN {
+    justify-content: center;
+    align-items: center;
+    width: 42px;
+    height: 100%;
+    > label {
+      display: flex;
+      align-items: center;
+      width: 100%;
+      height: 100%;
+      > svg {
+        padding: 10px;
+        width: 100%;
+        height: auto;
+      }
+    }
+  }
+
+  > .Header__AppName {
+    > span {
+      font-family: 'Montserrat';
+      font-weight: bold;
+      height: 20px;
+    }
+  }
+`;
