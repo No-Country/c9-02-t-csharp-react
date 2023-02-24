76dac83c7a7d14751bdbc4616c3e32abf6e62a03
Luis Mera <luismera86@gmail.com>
2023-02-24 16:17:16 -0300

    Add api in component services

--- front/MoonBank/src/MoonBankApp.tsx
+++ front/MoonBank/src/MoonBankApp.tsx
@@ -1,5 +1,6 @@
 import { Balance } from './operations/screens/Balance';
 import { Login } from './login/screens';
+import { Services } from './screens';
 import { getAccountsList } from './APIS/getRequest';
 
 const App = () => {
@@ -8,7 +9,7 @@ const App = () => {
     <>
       {/* <Login /> */}
       {/* <Balance/>   */}
-      {/* <Services /> */}
+      <Services />
     </>
   );
 };
--- front/MoonBank/src/operations/screens/Services.tsx
+++ front/MoonBank/src/operations/screens/Services.tsx
@@ -1,16 +1,34 @@
-import { Box, Container, ItemActivity, Paper, Text } from '../../shared/styles';
+import { Container, Paper, Text } from '../../shared/styles';
+import { useEffect, useState } from 'react';
 
 import { Activity } from '../components';
-import electricIcon from '../../shared/assets/eletricIcon.svg'
-import waterIcon from '../../shared/assets/waterIcon.svg'
+import { Service } from '../../shared/interfaces';
+import electricIcon from '../../shared/assets/eletricIcon.svg';
+import { getServicesList } from '../../APIS/getRequest';
+import waterIcon from '../../shared/assets/waterIcon.svg';
 
 export const Services = () => {
+  const [services, setServices] = useState<Service[]>([]);
+  useEffect(() => {
+    const data = getServicesList();
+    data.then((resp) => setServices(resp));
+  }, []);
+
   return (
     <Container>
       <Paper>
         <Text weight='700'>Services</Text>
-        <Activity icon={waterIcon} date='March 2023' quantity={15.00} serviceTitle='SPS Water Service' serviceDescription='Monthly Water Tax'/>
-        <Activity date='March 2023' icon={ electricIcon} quantity={31.89} serviceDescription='Montly Electricity tax' serviceTitle='ENEE'/>
+
+        {services.map((service, index) => (
+          <Activity
+            key={index}
+            date='March 2023'
+            icon={electricIcon}
+            quantity={service.amount}
+            serviceDescription={`Montly ${service.name} tax`}
+            serviceTitle={service.name}
+          />
+        ))}
       </Paper>
     </Container>
   );
