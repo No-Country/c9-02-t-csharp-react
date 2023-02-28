cbf566350208381856b2c92b9e7145df1cb918ea
Luis Mera <luismera86@gmail.com>
2023-02-28 17:39:52 -0300

    .

--- front/MoonBank/src/operations/screens/Services.tsx
+++ front/MoonBank/src/operations/screens/Services.tsx
@@ -26,8 +26,6 @@ export const Services = () => {
             serviceTitle={service.name}
             transaction={() => {}}
             typeItem='service'
-            
-
           />
         ))}
       </Paper>
--- front/MoonBank/src/shared/components/NavBar/NavBar.tsx
+++ front/MoonBank/src/shared/components/NavBar/NavBar.tsx
@@ -1,10 +1,9 @@
-import NavList from './Components/NavList/NavList';
-import NavHeader from './Components/NavHeader/NavHeader';
-import { PropsWithChildren } from 'react';
-import { NavBar_Styled } from './Styles/NavBar_Styled';
 import { NavBarProps } from './NavbarTypes';
-import { NavListElements } from './Components/NavList/NavListElements';
+import { NavBar_Styled } from './Styles/NavBar_Styled';
+import NavHeader from './Components/NavHeader/NavHeader';
+import NavList from './Components/NavList/NavList';
 import { useAppSelector } from '../../../store/hooks';
+
 function NavBar(props: NavBarProps) {
   const { rewardPoints, name, lastName } = useAppSelector(state => state.login)
   return (
