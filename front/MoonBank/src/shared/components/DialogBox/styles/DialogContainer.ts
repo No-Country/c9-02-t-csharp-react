317179769e2ff9035b3675acf5b4a29801079852
SortOmega <sortocarlo755@gmail.com>
2023-03-01 10:24:05 -0600

    Fix DialogBox Not Positioning correctly

--- front/MoonBank/src/shared/components/DialogBox/styles/DialogContainer.ts
+++ front/MoonBank/src/shared/components/DialogBox/styles/DialogContainer.ts
@@ -10,7 +10,7 @@ export const DialogContainer = styled.div<DialogContainerProp>`
       : css`
           display: flex;
         `}
-  position: absolute;
+  position: fixed;
   top: 0;
   left: 0;
   width: 100vw;
