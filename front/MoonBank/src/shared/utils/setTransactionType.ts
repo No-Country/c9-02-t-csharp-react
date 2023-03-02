578a5e75076a5e477a222d674b59f9a93c910321
Luis Mera <luismera86@gmail.com>
2023-03-02 16:47:05 -0300

    .

--- front/MoonBank/src/shared/utils/setTransactionType.ts
+++ front/MoonBank/src/shared/utils/setTransactionType.ts
@@ -1,8 +1,11 @@
+import { getAccountById } from '../../APIS/getRequest';
 import { useAppDispatch } from '../../store/hooks';
 
-const dispatch = useAppDispatch()
+const dispatch = useAppDispatch();
 
-export const setTransactionType = () => { 
+export const setTransactionType = (idDestination: number, typeTransaction: string) => {
+  
+  const userDestination = getAccountById(idDestination);
 
 
- }
+};
